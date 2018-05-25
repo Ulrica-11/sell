7b0ffe9de816a1f52e25922de2b34b3ff462f413
zkkblue <634511978@qq.com>
2018-05-25 13:33:32 +0800

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="header">
+      I am header!
+    </div>
+    <header></header>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+
+  export default {
+    components: {
+      header: header
+    }
+  };
+</script>
+
+<style>
+
+</style>
